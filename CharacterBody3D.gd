a069ae4a4b2653c9fb73f76a1f303bfaf6c6e1bf
TotallyJoel <joelheath64@gmail.com>
2024-04-02 16:58:14 -0400

    message

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,11 +1,17 @@
 extends CharacterBody3D
+@export var gravity := 50.0
+@export var speed := 8.0
+@export var jump_force :=20.0 
 
+@onready var spring_arm: SpringArm3D = $SpringArm3D
+@onready var mesh: MeshInstance3D = $MeshInstance3D
 
+var angular_acceleration := 7
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
 
 
 func _physics_process(delta):
@@ -21,9 +27,12 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, spring_arm.rotation.y).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x,direction.z), delta * angular_acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
